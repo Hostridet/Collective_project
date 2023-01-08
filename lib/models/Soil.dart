e6e26a2b12a3c8ee3e363e63818578a3ea18a6a5
Nikita Rumiantsev <nilita.rumyan@gmail.com>
2023-01-09 00:31:51 +0300

    создал модель почв

--- lib/models/Soil.dart
+++ lib/models/Soil.dart
@@ -1,5 +1,19 @@
 
 
 class Soil {
+  String type;
+  String subType;
+  String genus;
+  String sort;
+  String varieties;
+  String category;
 
+  Soil({
+    required this.type,
+    required this.subType,
+    required this.genus,
+    required this.sort,
+    required this.varieties,
+    required this.category,
+  });
 }
